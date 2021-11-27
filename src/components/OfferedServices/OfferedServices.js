ea11b29dd32f00bce13df1a53841379738572a8c
Kerim Nizamov <kerimnizamov@Kerims-MacBook-Air.local>
2021-11-27 19:01:13 +0200

    OfferedService component styling

--- src/components/OfferedServices/OfferedServices.js
+++ src/components/OfferedServices/OfferedServices.js
@@ -8,15 +8,15 @@ function OfferedServices() {
                 <ul>
                     <li className="services">
                         <p className="service-img"></p>
-                        <p className="service-description">WEB DEVELOPMENT <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nihil, accusantium dolorum dolore nesciunt aut. Beatae ut quasi aut, fuga placeat nihil quas cumque ipsam possimus porro voluptatem vel ab!</p>
+                        <p className="service-description"> <span className="service-header">WEB DEVELOPMENT</span> <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nihil, accusantium dolorum dolore nesciunt aut. Beatae ut quasi aut, fuga placeat nihil quas cumque ipsam possimus porro voluptatem vel ab!</p>
                     </li>
                     <li className="services">
-                        <p className="service-description">WEB DESIGN <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus optio dolores non. Perspiciatis dolorem quo praesentium, placeat sunt veniam! Nesciunt, ea. Possimus odit beatae atque harum dolorem provident explicabo veritatis.</p>
+                        <p className="service-description"> <span className="service-header">WEB DESIGN</span> <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus optio dolores non. Perspiciatis dolorem quo praesentium, placeat sunt veniam! Nesciunt, ea. Possimus odit beatae atque harum dolorem provident explicabo veritatis.</p>
                         <p className="service-img"></p>
                     </li>
                     <li className="services">
                         <p className="service-img"></p>
-                        <p className="service-description">NESHTO SI DRUGO <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, repellat illum eum necessitatibus dolor dolores, corporis expedita blanditiis perspiciatis impedit quis? Itaque sed, aut quibusdam molestiae harum odio quae hic.</p>
+                        <p className="service-description"> <span className="service-header">NESHTO SI DRUGO</span> <br />Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, repellat illum eum necessitatibus dolor dolores, corporis expedita blanditiis perspiciatis impedit quis? Itaque sed, aut quibusdam molestiae harum odio quae hic.</p>
                     </li>
                 </ul>
             </div>
